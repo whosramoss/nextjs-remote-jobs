0f824e7bb43d5a9f061ee06d9e72efe3621d55c1
Gabriel Ramos <whosramoss@gmail.com>
2025-02-04 20:34:57 -0300

    chore: remove files

---
+++ src/hooks/useActiveId.ts
@@ -0,0 +1,23 @@
+"use client"
+import { useState, useEffect } from "react";
+
+export function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      const id = +window.location.hash.slice(1);
+      setActiveId(id);
+    };
+    handleHashChange();
+
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
+  return activeId;
+}
+
