56e52ba486fc1febcbb609695a06371519fa3dbe
Gabriel Ramos <whosramoss@gmail.com>
2025-02-04 20:37:00 -0300

    chore: update utils

--- src/utils/app-metadata.ts
+++ src/utils/app-metadata.ts
@@ -9,9 +9,9 @@ export default class AppMetadata {
 
   constructor(isLocalTest: boolean) {
     this.isLocalTest = isLocalTest;
-    this.META_URL = "https://github.com/whosramoss/nt3/";
-    this.META_NAME = "NT3 - NextJs Boilerplate";
-    this.META_DESCRIPTION = "Next.js Boilerplate to creative projects";
+    this.META_URL = "https://github.com/whosramoss/nextjs-remote-jobs/";
+    this.META_NAME = "Remote Jobs";
+    this.META_DESCRIPTION = "Remote Jobs Dashboard";
     this.META_SITENAME = this.META_URL;
     this.META_THUMBNAIL = `${this.META_URL}thumbnail.png`;
     this.META_BASE = isLocalTest ? "http://localhost:3000/" : this.META_URL;
